84ca1524cd59342d8118033510d7e1a3206dbdca
Sabitov Tirek <153196289+tikkon66@users.noreply.github.com>
2026-07-09 23:52:18 +0500

    Change CORS origin for the application
    Change front path

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin: "https://tetris-front.vercel.app"
+    origin: "https://tikkon66.github.io" 
 }));
 app.use(express.json());
 
