efffae56df9916c29d96a47d8528ae9f58f47e74
Sabitov Tirek <153196289+tikkon66@users.noreply.github.com>
2026-07-14 03:34:18 +0500

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,10 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // Позволяет подключаться к Supabase напрямую без локального файла сертификата
+    rejectUnauthorized: false 
   }
 });
 
-// Проверка подключения при старте, чтобы сразу видеть ошибки в логах Render
 pool.connect((err, client, release) => {
   if (err) {
     return console.error("=== [PG CONNECT ERROR]: Ошибка подключения к базе ===", err.message);
